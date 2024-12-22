e029ebf66e462006b0aedb93a144a5eb8cc7913f
Viktor Ivanov <ivikspb@gmail.com>
2024-12-22 21:43:50 +0300

    init

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Magic UI (Cool mode)",
+    "summary": "Magic UI (Cool mode)",
+    "description": """Magic UI (Cool mode)""",
+    "author": "ivik",
+    "category": "Web",
+    "version": "18.0.0.1",
+    "license": "LGPL-3",
+    "depends": [
+        "web",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "magicui_coolmode_odoo/static/src/js/coolmode-service.js",
+        ],
+    },
+    "application": True,
+}
